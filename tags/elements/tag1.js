b34a20bcf7d63cf0722db26dc49589e05bd25f03
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-10-26 20:51:33 -0700

    started building the tag/CMS system

---
+++ tags/elements/tag1.js
@@ -0,0 +1,8 @@
+function tag1(){
+    document.getElementById("tag1").innerHTML = '<center><a href="./index.html"><h3>JustFacts</h3></a> <p> <a href="./index.html"><button class="btn-dark-solid">Latest</button></a><a href="./about.html"><button style="margin-left: 5px;" class="btn-dark-solid">About</button></a> </p></center>'
+}
+
+
+function tag2(){
+    document.getElementById("tag2").innerHTML = '<center><a href="https://www.borgcreative.com"> <img src="./img/borg.png" width="90px"> </a></center>'
+}
---
+++ tags/reports/tag1.js
@@ -0,0 +1,3 @@
+function report1(){
+    document.getElementById("report1").innerHTML = '<center><div class="card"><p class="card-header">Liberty.CSS</p><p class="card-author">Jacob Borg</p><p class="card-body">A new CSS library</p></div></center>'
+}
